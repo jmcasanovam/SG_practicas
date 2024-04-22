a4c93f6fa5b54d7bad500bcadc184b5e121190ef
jmcasanovam <6jmcasanovam@gmail.com>
2024-04-22 17:17:55 +0200

    creo el cartel de suelo mojado

---
+++ cartel/Cartel.js
@@ -0,0 +1,98 @@
+
+import * as THREE from 'three'
+import { CSG } from '../libs/CSG-v2.js';
+
+class Cartel extends THREE.Object3D {
+    constructor(gui, titleGui) {
+        super();
+
+        this.createGUI(gui, titleGui);
+
+        var cartel = this.createCartelCompleto();
+        this.add(cartel);
+    }
+
+    createCartelCompleto() {
+        const giro = Math.PI / 8;
+        var cartel = new THREE.Object3D();
+        var cartel1 = this.createFiguraCartel();
+        var cartel2 = this.createFiguraCartel();
+        cartel2.position.set(0, 0, 0.8);
+        cartel1.rotation.x = giro;
+        cartel2.rotation.x = -1*giro;
+        cartel.add(cartel1);
+        cartel.add(cartel2);
+
+        var caja = new THREE.BoxGeometry(0.01, 0.05, 0.5);
+        var material = new THREE.MeshStandardMaterial({ color: 0x808080, metalness: 1, roughness: 0.9 });
+        var cajaMesh = new THREE.Mesh(caja, material);
+        cajaMesh.position.set(0.01, 0.35, 0.25+0.17);
+        cartel.add(cajaMesh);
+        var caja2 = new THREE.BoxGeometry(0.01, 0.05, 0.5); 
+        var cajaMesh2 = new THREE.Mesh(caja2, material);
+        cajaMesh2.position.set(0.49, 0.35, 0.25 + 0.17);
+        cartel.add(cajaMesh2);
+        return cartel;
+    }
+
+    createFiguraCartel() {
+        var cartelShape = this.createCartelShape();
+        var options1 = { depth: 0.05, steps: 1, bevelEnabled: false };
+        var cartelGeometry = new THREE.ExtrudeGeometry(cartelShape, options1);
+        const material = new THREE.MeshStandardMaterial({ color: 0xffff00 });
+        var cartel = new THREE.Mesh(cartelGeometry, material);
+        return cartel;
+    }
+
+
+    createCartelShape() {
+        const altura = 1;
+        const anchura = 0.5;
+        const x = 0;
+        const y = 0;
+        var shape = new THREE.Shape();
+        //Se crea el contorno exterior
+        shape.moveTo(x, y);
+        shape.lineTo(anchura * 0.2, y);
+        shape.lineTo(anchura * 0.2, altura * 0.2);
+        shape.lineTo(anchura * 0.8, altura * 0.2);
+        shape.lineTo(anchura * 0.8, y);
+        shape.lineTo(anchura, y);
+        shape.lineTo(anchura, altura);
+        shape.lineTo(anchura * 0.8, altura);
+        shape.lineTo(anchura * 0.8, altura * 0.8);
+        shape.lineTo(anchura * 0.2, altura * 0.8);
+        shape.lineTo(anchura * 0.2, altura);
+        shape.lineTo(x, altura);
+        shape.lineTo(x, y);
+        
+        return shape;
+    }
+
+
+
+    createGUI(gui, titleGui) {
+        // Controles para el movimiento de la parte móvil
+        this.guiControls = {
+            rotacion: 0
+        }
+
+        // Se crea una sección para los controles de la caja
+        var folder = gui.addFolder(titleGui);
+        // Estas lineas son las que añaden los componentes de la interfaz
+        // Las tres cifras indican un valor mínimo, un máximo y el incremento
+        folder.add(this.guiControls, 'rotacion', -0.125, 0.2, 0.001)
+            .name('Apertura : ')
+            .onChange((value) => this.setAngulo(-value));
+    }
+
+    setAngulo(valor) {
+        this.movil.rotation.z = valor;
+    }
+
+    update() {
+        // No hay nada que actualizar ya que la apertura de la grapadora se ha actualizado desde la interfaz
+    }
+}
+
+export { Cartel }
--- cartel/MyScene.js
+++ cartel/MyScene.js
@@ -8,7 +8,7 @@ import { Stats } from '../libs/stats.module.js'
 
 // Clases de mi proyecto
 
-import { Pastilla } from './Pastilla.js'
+import { Cartel } from './Cartel.js'
 
 
 /// La clase fachada del modelo
@@ -49,7 +49,7 @@ class MyScene extends THREE.Scene {
         // Por último creamos el modelo.
         // El modelo puede incluir su parte de la interfaz gráfica de usuario. Le pasamos la referencia a 
         // la gui y el texto bajo el que se agruparán los controles de la interfaz que añada el modelo.
-        this.model = new Pastilla(this.gui, "Controles de la Pastilla");//this.gui, "Controles de la Geometría"
+        this.model = new Cartel(this.gui, "Controles del cartel");//this.gui, "Controles de la Geometría"
         this.add(this.model);
     }
 
