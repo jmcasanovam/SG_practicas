c0356b6e8e04b63fd7a3f2a60b729e0146169b03
jmcasanovam <6jmcasanovam@gmail.com>
2024-05-04 18:29:19 +0200

    alejo el plano far de la camara para que se vea mejor y situo la camara de forma más generica

--- juego/MyScene.js
+++ juego/MyScene.js
@@ -58,9 +58,9 @@ class MyScene extends THREE.Scene {
     //   El ángulo del campo de visión vértical en grados sexagesimales
     //   La razón de aspecto ancho/alto
     //   Los planos de recorte cercano y lejano
-    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 10);
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 30);
     // También se indica dónde se coloca
-    this.camera.position.set (0.2, 0.05, 0.2);
+    this.camera.position.set (6.2, 0.05, 6.2);
     // Y hacia dónde mira
     var look = new THREE.Vector3 (0,0,0);
     this.camera.lookAt(look);
